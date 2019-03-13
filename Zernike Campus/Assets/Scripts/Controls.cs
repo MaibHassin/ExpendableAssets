798b49ba5fc4f7513adf3b8631400d3cee88b6d7
Maib Hassin Syed <maibhassin@gmail.com>
2019-03-14 00:58:38 +0100

    Pinch to zoom in and out
    - Pinch on touch screen with 2 fingers to zoom in and viceversa

--- Zernike Campus/Assets/Scripts/Controls.cs
+++ Zernike Campus/Assets/Scripts/Controls.cs
@@ -17,7 +17,7 @@ public class Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) //if one or more finger is on screen and the finger moved then
+        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) //if one or more finger is on screen and the finger moved then
         {
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition; //get the touch position
             transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0); //move camera in opposite direction of drag
@@ -28,6 +28,22 @@ public class Controls : MonoBehaviour
                 Mathf.Clamp(transform.position.x, -950.0f, 1547.0f),
                 Mathf.Clamp(transform.position.y, 984.0f, 1589.0f),
                 Mathf.Clamp(transform.position.z, -3603.94f, -3603.94f));
+        } else if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float previousTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagDiff = previousTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.fieldOfView += deltaMagDiff * 0.1f;
+
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 25.0f, 100.0f);
         }
     }
 }
