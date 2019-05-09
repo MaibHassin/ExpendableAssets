36046adc78a52c8f1676252ef01c11d581420411
Meap123 <korf.teun@gmail.com>
2019-05-09 15:01:20 +0200

    clicking buildings opens information

---
+++ Zernike Campus/Assets/Scripts/buildingClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingClick : MonoBehaviour
+{
+    public OpenBuildingsList OpenBuilding;
+
+
+    private void OnMouseDown()
+    {
+        OpenBuilding.OpenInfoPanel();
+    }
+}
